23c56ef2366debaf75e7b8db2d32971f3a823654
George Mulhearn <gmulhearn@anonyome.com>
2025-04-02 10:52:53 +1000

    fix
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>

--- did_core/did_doc/src/schema/mod.rs
+++ did_core/did_doc/src/schema/mod.rs
@@ -7,6 +7,7 @@ pub mod verification_method;
 /// Module of commonly used DID-related JSON-LD contexts
 pub mod contexts {
     pub const W3C_DID_V1: &str = "https://www.w3.org/ns/did/v1";
+    pub const W3C_DID_V1_ALT: &str = "https://w3id.org/did/v1";
     pub const W3C_SUITE_ED25519_2020: &str = "https://w3id.org/security/suites/ed25519-2020/v1";
     pub const W3C_SUITE_ED25519_2018: &str = "https://w3id.org/security/suites/ed25519-2018/v1";
     pub const W3C_SUITE_JWS_2020: &str = "https://w3id.org/security/suites/jws-2020/v1";
